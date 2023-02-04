2a110236db094d19fee12c1e99d228b04c7280c1
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2023-02-04 02:34:49 -0500

    Player Movement
    jump
    up
    down
    grid snap

--- GlobalGameJamRepository/Assets/JaneAssets/Scripting/PlayerMovement.cs
+++ GlobalGameJamRepository/Assets/JaneAssets/Scripting/PlayerMovement.cs
@@ -4,38 +4,103 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 3; // Initial player movement forward speed
     public float upAndDownSpeed = 3; // Player's up and down speed
+    public bool canMove;
+    
+    Vector3 up = Vector3.zero, down = new Vector3(0, 1.5f, -8), currentDirection = Vector3.zero;
+    Vector3 nextPosition, destination, direction;
 
+    float rayLength = 1f;
+
+    public Rigidbody rigidBody;
+    public Vector3 jump;
+    public float jumpAmt = 2.0f;
+
+    public bool isGrounded;
+   
+    void Start()
+    {
+        currentDirection = up;
+        nextPosition = Vector3.left;
+        destination = transform.position;
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    
 
     void Update() // Update is called once per frame
 
     {
-        // Forward movement
+        Move();
+        
+    }
 
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-
-        // Up movement
-
-        if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.UpArrow))) 
+    private void OnCollisionEnter(Collision gameObj)
+    {
+        if (gameObj.gameObject.tag == "Ground")
         {
-            if (this.gameObject.transform.position.x > LevelBoundary.upSide)
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * upAndDownSpeed);
-            }
-        }
-
-        // Down movement
-
-        if (Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.DownArrow))) 
-        {
-            if (this.gameObject.transform.position.x < LevelBoundary.downSide)
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * upAndDownSpeed * -1);
-            }
+            isGrounded = true;
         }
 
     }
 
-    
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, upAndDownSpeed * Time.deltaTime);
+
+        // Movement
+        if ((Input.GetKeyDown(KeyCode.W))) 
+        {
+            nextPosition = Vector3.left;
+            currentDirection = Vector3.left;
+            canMove = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.S)))
+        {
+            nextPosition = Vector3.right;
+            currentDirection = Vector3.right;
+            canMove = true;
+        }
+
+        if (Vector3.Distance(destination,transform.position) <= 0.00001f)
+        {
+            transform.localEulerAngles = currentDirection;
+            if (canMove)
+            {
+                if (Valid())
+                {
+                    destination = transform.position + nextPosition;
+                    direction = nextPosition;
+                    canMove = false;
+                }            
+            }
+        }
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("this enters sexy lady");
+
+            rigidBody.AddForce(jump * jumpAmt, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        bool Valid()
+        {
+            Ray myRay = new Ray(transform.position + new Vector3(0, 0.25f, 0), transform.forward);
+            RaycastHit hit;
+
+            Debug.DrawRay(myRay.origin, myRay.direction, Color.red);
+
+            if(Physics.Raycast(myRay,out hit,rayLength))
+            {
+                if(hit.collider.tag == "wall")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
