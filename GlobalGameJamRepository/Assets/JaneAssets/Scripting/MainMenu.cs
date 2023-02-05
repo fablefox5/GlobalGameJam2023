0e8482d6d5bc362adb50e888cd3f7d11957df70a
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2023-02-05 02:16:34 -0500

    Updated UI and Main Menu
    ask jane for what goes in whaat

--- GlobalGameJamRepository/Assets/JaneAssets/Scripting/MainMenu.cs
+++ GlobalGameJamRepository/Assets/JaneAssets/Scripting/MainMenu.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
     bool startGame = false;
     bool isPaused = false;
+    [SerializeField] public Image imageToSwap;
+    [SerializeField] public Sprite playImage;
+    [SerializeField] public Sprite pauseImage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,13 +29,17 @@ public class MainMenu : MonoBehaviour
     {
         if (isPaused == false)
         {
+           
             isPaused = true;
             Time.timeScale = 0;
+            imageToSwap.overrideSprite = playImage;
         }
         else
         {
             isPaused = false;
             Time.timeScale = 1;
+            imageToSwap.overrideSprite = pauseImage;
+
         }
     }
 
