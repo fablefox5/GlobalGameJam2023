5ee1e0de741030943f8cae304e11b979b285ba52
FrankSilantro <thatoneguy2439@gmail.com>
2023-02-05 01:31:39 -0500

    Spin Animation Complete

--- GlobalGameJamRepository/Assets/ZiadAssets/Chancla.cs
+++ GlobalGameJamRepository/Assets/ZiadAssets/Chancla.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Chancla : MonoBehaviour
 {
-    // Start is called before the first frame update
     void ChanclaDestroy()
     {
         Destroy(this.gameObject);
