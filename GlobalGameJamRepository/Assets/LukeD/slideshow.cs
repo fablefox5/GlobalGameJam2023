1ab369f423db1e1e6c62107bfba26e6eb09077dd
luke <58479519+lukedobbs@users.noreply.github.com>
2023-02-04 23:46:51 -0500

    Slideshow added

--- GlobalGameJamRepository/Assets/LukeD/slideshow.cs
+++ GlobalGameJamRepository/Assets/LukeD/slideshow.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class slideshow : MonoBehaviour
 {
+    public int imageIndex = 0;
+    public Image slide;
+    public Sprite pic1, pic2, pic3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            imageIndex++;
+        }
+        if(imageIndex == 1)
+        {
+            slide.overrideSprite = pic1;
+        }
+        else if (imageIndex == 2)
+        {
+            slide.overrideSprite = pic2;
+        }
+        else if (imageIndex == 3)
+        {
+            slide.overrideSprite = pic3;
+        }
+        else if (imageIndex > 3)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    IEnumerator begin()
+    {
+       
+        yield return null;
     }
 }
