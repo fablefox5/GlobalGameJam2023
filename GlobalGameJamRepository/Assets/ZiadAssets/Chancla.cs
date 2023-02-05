baa19928efe3a790d7f5af2b4e9f5f1576639a51
FrankSilantro <thatoneguy2439@gmail.com>
2023-02-04 21:12:01 -0500

    4 house generation working
    Chancla class added

---
+++ GlobalGameJamRepository/Assets/ZiadAssets/Chancla.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chancla : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void ChanclaDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
