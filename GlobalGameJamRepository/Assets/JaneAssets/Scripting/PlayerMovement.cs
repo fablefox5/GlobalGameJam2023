5f9307bdae8f20c757d15d12c60929a6f6d3f2a7
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2023-02-04 19:03:37 -0500

    Updated player movement and animations
    this movement should be good, need a work around to dying

--- GlobalGameJamRepository/Assets/JaneAssets/Scripting/PlayerMovement.cs
+++ GlobalGameJamRepository/Assets/JaneAssets/Scripting/PlayerMovement.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float upAndDownSpeed = 3; // Player's up and down speed
-    public bool canMove;
+    public bool canMove = true;
+    public bool canMoveForward;
+    public bool canMoveBack;
     
     Vector3 up = Vector3.zero, down = new Vector3(0, 1.5f, -8), currentDirection = Vector3.zero;
     Vector3 nextPosition, destination, direction;
 
-    float rayLength = 1f;
-
     public Rigidbody rigidBody;
-
-    //public Vector3 jump;
-    //public float jumpAmt = 2.0f;
     public bool isGrounded; // Boolean that tells us if the player is on the ground
    
     void Start()
@@ -29,10 +26,8 @@ public class PlayerMovement : MonoBehaviour
 
     
     void Update() // Update is called once per frame
-
     {
         Move();
-        
     }
 
     private void OnCollisionEnter(Collision gameObj)
@@ -48,39 +43,61 @@ public class PlayerMovement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, destination, upAndDownSpeed * Time.deltaTime);
 
         // Movement
-        
-        if ((Input.GetKeyDown(KeyCode.W)) && canMove) 
+
+        if (transform.position.x > 1 && transform.position.x <= 1.5 && canMove)
         {
+            canMoveForward = true;
+            canMoveBack = true;
+        }
+        else if (transform.position.x > 0  && transform.position.x <= 1)
+        {
+            canMoveForward = false;
+            canMoveBack = true;
+        }
+        else if (transform.position.x > 1.5 && transform.position.x <= 2)
+        {
+            canMoveForward = true;
+            canMoveBack = false;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.W)) && canMoveForward)
+        {
+
             nextPosition = Vector3.left;
             currentDirection = Vector3.left;
-            //canMove = true;
+            canMove = true;
+
         }
-        if ((Input.GetKeyDown(KeyCode.S)) && canMove)
+        if ((Input.GetKeyDown(KeyCode.S)) && canMoveBack)
         {
+
             nextPosition = Vector3.right;
             currentDirection = Vector3.right;
-            //canMove = true;
+            canMove = true;
+
         }
+
 
         if (Vector3.Distance(destination,transform.position) <= 0.00001f)
         {
-            //transform.localEulerAngles = currentDirection;
+            Debug.Log("this works");
+
             if (canMove)
             {
                 if (Valid())
                 {
+
                     destination = transform.position + nextPosition;
                     direction = nextPosition;
                     canMove = false;
-                }            
+                }
             }
+
         }
 
         // Jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // When the player is pressing space and is on the ground
-        {
-            // rigidBody.AddForce(jump * jumpAmt, ForceMode.Impulse);
-            
+        {            
             Debug.Log("jump works");
 
             isGrounded = false; // The player is no longer on the ground
@@ -91,19 +108,11 @@ public class PlayerMovement : MonoBehaviour
         bool Valid()
         {
             Ray myRay = new Ray(transform.position + new Vector3(0, 0.25f, 0), transform.forward);
-            RaycastHit hit;
-
-            Debug.DrawRay(myRay.origin, myRay.direction, Color.red);
-
-            if(Physics.Raycast(myRay,out hit,rayLength))
-            {
-                if(hit.collider.tag == "wall")
-                {
-                    return false;
-                }
-            }
+            
+           
             return true;
         }
+
 
         IEnumerator JumpCoolDown() { // When jumping
             Debug.Log("jump coroutine entered");
---
+++ GlobalGameJamRepository/Assets/JaneAssets/Scripting/ScoreKeeper.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+
+    public Text scoreText; // connect reference in inspector
+    public float scoreAmount;
+    public float pointIncreasedPerSecond;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreAmount = 0f;
+        pointIncreasedPerSecond = 1f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score" + (int) scoreAmount;
+        scoreAmount += pointIncreasedPerSecond * Time.deltaTime;        
+    }
+}
