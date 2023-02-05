fee4378cbbb96a0c04f27536be3179efbf39bb46
FrankSilantro <thatoneguy2439@gmail.com>
2023-02-05 02:41:34 -0500

    ChanclaSpawnerCreated

---
+++ GlobalGameJamRepository/Assets/ZiadAssets/ChanclaSpawnFolder/ChanclaSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChanclaSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject chanclaType;
+    private int randomNumber;
+    void Start()
+    {
+        InvokeRepeating("RandomSpawnChancla", 8f, 3f);
+    }
+    private void RandomSpawnChancla()
+    {
+        Debug.Log("numbergenerated");
+        randomNumber = Random.Range((int)1, (int)10);
+        if (randomNumber >= 1 && randomNumber <= 3)
+        {
+            Instantiate(chanclaType);
+            Debug.Log("InstantiateChancla");
+        }
+    }
+}
